0255acf361cc9b85dc996dc4f2387dc6bf0b64b7
bala-amypo <bala@amypo.in>
2025-12-17 09:28:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/LocationEntity.java
+++ src/main/java/com/example/demo/entity/LocationEntity.java
@@ -4,58 +4,45 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "location")
 public class LocationEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private Double latitude;
     private Double longitude;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
-    public LocationEntity() {
+    public String getName() {
+        return name;
     }
-public LocationEntity(Long id, String name, Double latitude, Double longitude) {
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+    public Double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+    public LocationEntity(Long id, String name, Double latitude, Double longitude) {
         this.id = id;
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
     }
+    public LocationEntity() {
+    }           
 }
