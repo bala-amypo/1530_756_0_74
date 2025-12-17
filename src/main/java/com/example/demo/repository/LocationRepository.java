1f070979b29c04012c736a2ba314c4bab14bef6e
bala-amypo <bala@amypo.in>
2025-12-17 06:26:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/LocationRepository.java
+++ src/main/java/com/example/demo/repository/LocationRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
 public interface LocationEntity{
-    LocationEntity createlocation(LocationEntity);
+    LocationEntity createlocation(LocationEntity location);
+    List<LocationEntity>getallocation
 }
